051314098d9d30e52f80f381dac32c5486aeeec2
flip32 <fl1p3r@gmail.com>
2019-03-30 11:35:37 -0300

    alteracao de layout

--- src/app/home/mock.ts
+++ src/app/home/mock.ts
@@ -1,10 +1,10 @@
 import {Card} from '../shared/card.model';
 
 export const CARDS: Card[] = [
-    {titulo: 'Trilhas', img: '../../assets/icones/trilhas.png'},
-    {titulo: 'Ocorrênias', img: '../../assets/icones/ocorrencias.png'},
-    {titulo: 'Atrações', img: '../../assets/icones/atracoes.png'},
-    {titulo: 'IcmBio', img: '../../assets/icones/icmbio.png'},
-    {titulo: 'Emergência', img: '../../assets/icones/emergencias.png'},
-    {titulo: 'Avisos', img: '../../assets/icones/avisos.png'},
+    {titulo: 'Trilhas', img: '../../assets/icones/trilhas.png', url: '/trilhas'},
+    {titulo: 'Ocorrênias', img: '../../assets/icones/ocorrencias.png', url: '/ocorrencias'},
+    {titulo: 'Atrações', img: '../../assets/icones/atracoes.png', url: '/atracoes'},
+    {titulo: 'IcmBio', img: '../../assets/icones/icmbio.png',  url: '/icmbio'},
+    {titulo: 'Emergência', img: '../../assets/icones/emergencias.png',  url: '/emergencia'},
+    {titulo: 'Avisos', img: '../../assets/icones/avisos.png',  url: '/avisos'},
 ];
--- src/app/shared/card.model.ts
+++ src/app/shared/card.model.ts
@@ -1,4 +1,5 @@
 export class Card {
     public titulo: string;
     public img: string;
+    public url: string;
 }
